e278727216e862db7645a25e5eac522149cc18f9
vibeforge1111 <vibeforge1111@gmail.com>
2026-04-27 23:20:02 +0400

    Pin installer to latest Spark CLI

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Prefix = "$HOME\.spark",
     [string]$Source = "https://github.com/vibeforge1111/spark-cli",
-    [string]$Ref = "a3cf00f752a5093b0fafebd7168ea82c4b787075",
+    [string]$Ref = "06ca31f4ace233a8066e2fe8016d86273711103b",
     [string]$NodeVersion = "22.18.0",
     [string]$PythonVersion = "3.11",
     [string]$UvVersion = "0.11.7",
@@ -29,7 +29,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$SparkCliReleaseName = "spark-cli-launch-2026-04-27-8"
+$SparkCliReleaseName = "spark-cli-launch-2026-04-27-9"
 $RefWasProvided = $PSBoundParameters.ContainsKey("Ref")
 $Script:InstallLockDir = ""
 $Script:PythonExe = ""
